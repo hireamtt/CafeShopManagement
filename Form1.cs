e583bb4dd7f63d0ce0eb9f48c19963d08a494372
mahesh gawande <maheshsg297@gmil.com>
2021-05-06 00:20:41 +0530

    changes by mahesh.gawande (extra code removal)

--- Form1.cs
+++ Form1.cs
@@ -17,37 +17,7 @@ namespace CafeShopManagement
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
         }
